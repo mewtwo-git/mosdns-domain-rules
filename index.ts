984191bf11a8e546f71f63144022211d1261bbb3
mewtwo-git <80144566+mewtwo-git@users.noreply.github.com>
2023-05-16 21:54:35 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,65 @@
+import axios from "axios";
+import * as fs from "fs";
+import { writeFileSync } from "fs-extra";
+import { removeSync } from "fs-extra";
+import { pathExistsSync } from "fs-extra";
+import os from "os";
+
+const aliUrls = [
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Alibaba/Alibaba.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/AliPay/AliPay.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/CaiNiao/CaiNiao.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Eleme/Eleme.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/FeiZhu/FeiZhu.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Himalaya/Himalaya.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/XianYu/XianYu.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/XiamiMusic/XiamiMusic.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Youku/Youku.list",
+];
+
+const txUrls = [
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Tencent/Tencent.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/TencentVideo/TencentVideo.list",
+  "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/WeChat/WeChat.list",
+];
+
+const getUrlRaw = async (url: string): Promise<string> => {
+  const resp = await axios.get(url);
+  return resp.data;
+};
+
+const convertRaw = (data: string) => {
+  const result: string[] = [];
+  const arrStr = data.split(os.EOL);
+  arrStr.forEach((element) => {
+    if (element.startsWith("DOMAIN-SUFFIX,") && element.includes(".")) {
+      result.push(element.replace("DOMAIN-SUFFIX,", "domain:"));
+    } else if (element.startsWith("DOMAIN,")) {
+      result.push(element.replace("DOMAIN,", "full:"));
+    }
+  });
+  return result;
+};
+
+const main = async (urls: string[], customRules: string[], fileName: string) => {
+  const promises = urls.map((url) => getUrlRaw(url));
+  const resps = await Promise.all(promises);
+  let arrStr: string[] = customRules;
+  resps.forEach((resp) => {
+    arrStr = arrStr.concat(convertRaw(resp));
+  });
+  if (!fs.existsSync("dist")) {
+    fs.mkdirSync("dist");
+  }
+  writeFileSync(fileName, [...new Set(arrStr)].sort().join(os.EOL));
+};
+
+(async () => {
+  await main(aliUrls, [
+    "domain:mybank.cn"
+  ], "dist/ali.txt");
+  await main(txUrls, [
+    "domain:sogou.com",
+    "domain:sogoucdn.com"
+  ], "dist/tx.txt");
+})();
